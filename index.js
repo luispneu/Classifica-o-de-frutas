3c7d8b04f78241f5b90b3076419892ac267e6d6d
luispneu <luisgabriel8328@gmail.com>
2024-11-14 15:56:49 -0300

    configurando envio dos dados para o front

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const prisma = require("./prisma/prismaClient");
 
 const express = require("express")
-
+const cors = require('cors');
 const app =  express();
 app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use(express.urlencoded({ extended: true}));
 
